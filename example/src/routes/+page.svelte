943572282af081941bafbf825dd738006ad13cd9
Hugo Villagrana <villagrana.hugo1@gmail.com>
2024-07-09 20:04:58 -0400

    add svelte app

---
+++ example/src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { getMessage } from '$lib/greeting/client';
+	let greeting: string = 'Press Button For a Greeting';
+
+	const handleClick = async () => {
+		sendMessage();
+	};
+
+	const sendMessage = async () => {
+		try {
+			const res = await getMessage('test');
+			greeting = res;
+		} catch (err) {
+			const error = err instanceof Error ? err.message : 'An unknown error occurred';
+			console.error(error);
+		}
+	};
+</script>
+
+<p>{greeting}</p>
+<button on:click={handleClick}>Generate Greeting!</button>
