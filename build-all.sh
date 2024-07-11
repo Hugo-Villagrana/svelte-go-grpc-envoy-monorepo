cef81d087959c15093b19a69c16eaef4e15483a7
Hugo Villagrana <villagrana.hugo1@gmail.com>
2024-07-10 21:41:31 -0400

    fix services

--- build-all.sh
+++ build-all.sh
@@ -3,5 +3,5 @@
 # Build Envoy Proxy
 docker build -t envoy-proxy -f ./envoy/Dockerfile ./envoy
 
-# Build WealthSnap App
-docker build -t example_service -f ./services/example_service/Dockerfile --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_ed25519)" .
+# Build Example Monorepo App
+docker build -t example_monorepo -f ./services/example_monorepo/Dockerfile --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_ed25519)" .
